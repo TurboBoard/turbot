7b221567b77959244c1c76811eb025b0b87357ba
Michael Krukar <krukar@gmail.com>
2023-12-05 18:40:21 -0500

    Basic Discord bot setup

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+client.login(process.env.DISCORD_TOKEN);
+//# sourceMappingURL=index.js.map
